a4c85081baa1df1a1339a3bedf10709be4fc4f71
LeonardJAvi <informatic.leonardavila@gmail.com>
2018-06-25 10:29:00 -0400

    Changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+// var haml = require('gulp-ruby-haml');
+var haml = require('gulp-haml');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: './src'
+    }
+  })
+});
 
 gulp.task('prueba', function () {
   return gulp.src('./src/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/assets/css'));
+    .pipe(gulp.dest('./docs/assets/css'));
 });
 
 
+gulp.task('prueba2', function () {
+  gulp.src('./src/**/*.haml')
+    .pipe(haml())
+    .pipe(gulp.dest('./docs'));
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch('./src/scss/*.scss').on('change', browserSync.reload) 
+  gulp.watch('./src/*.haml').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['prueba', 'prueba2','watch','serve',])
